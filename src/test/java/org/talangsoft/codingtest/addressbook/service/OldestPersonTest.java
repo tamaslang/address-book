5b330b0473e4432c8f954fc314f890c149105adb
Tamas Lang <tamas.lang@talangsoft.org>
2015-04-07 16:21:19 +0100

    add 1 person db test for oldest person -> fails

--- src/test/java/org/talangsoft/codingtest/addressbook/service/OldestPersonTest.java
+++ src/test/java/org/talangsoft/codingtest/addressbook/service/OldestPersonTest.java
@@ -1,5 +1,6 @@
 package org.talangsoft.codingtest.addressbook.service;
 
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +9,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.talangsoft.codingtest.addressbook.domain.AddressBook;
 import org.talangsoft.codingtest.addressbook.domain.Gender;
+import org.talangsoft.codingtest.addressbook.domain.Person;
 import org.talangsoft.codingtest.addressbook.test.AddressBookTestContext;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
@@ -37,6 +40,14 @@ public class OldestPersonTest {
     @Test
     public void getOldestPersonForEmpty(){
         assertNull(service.getOldestPerson());
+    }
+
+
+    @Test
+    public void getOldestPersonFor1PersonDB(){
+        Person onlyPerson = new Person("Sarah","Stone",Gender.FEMALE,new DateTime(1977,11,11,11,11,11));
+        addressBook.addPerson(onlyPerson);
+        assertEquals(onlyPerson,service.getOldestPerson());
     }
 
 }
